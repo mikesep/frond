fdee96fd9e8b68befb810efbceaad8c678228a4b
Mike Seplowitz <mseplowitz@bloomberg.net>
2021-02-05 16:24:18 -0500

    Fix local repo test

--- internal/git/localrepo_test.go
+++ internal/git/localrepo_test.go
@@ -6,8 +6,8 @@ import (
 	"github.com/mikesep/frond/internal/git"
 )
 
-func Test_LocalRepoAtDir(t *testing.T) {
-	r, err := git.LocalRepoAtDir("../../")
+func Test_IsLocalRepoRoot(t *testing.T) {
+	r, err := git.IsLocalRepoRoot("../../")
 	t.Logf("r = %+v", r)
 	t.Logf("err = %v", err)
 }
